9ee31f8a9a0b298f3289fb347c2bda01ba11bcb1
maxstar <maksimgavura78@gmail.com>
2024-12-08 23:04:53 +0200

    Добавьте файлы проекта.

---
+++ Lesson#5Task2/Lesson_5Task2.py
@@ -0,0 +1,17 @@
+﻿vowel = 'aeiou'
+consonant = 'bcdfghjklmnpqrstvwxyz'
+
+word = input('Vvedite clovo: ').lower()
+
+vowel_count = 0
+consonant_count = 0
+
+for char in word:
+    if char in vowel:
+        vowel_count += 1
+    elif char in consonant:
+        consonant_count += 1
+    elif (word != vowel) and (word != consonant):
+        print('false')
+
+print(f'kolichestvo glasnyh: {vowel_count}',f'kolichestco soglasnyh: {consonant_count}')
